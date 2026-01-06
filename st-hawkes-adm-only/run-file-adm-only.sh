095fbb1bf2146527cda7fa73316579083f4ebba1
Raiha Browning <raiha_p123@hotmail.com>
2026-01-06 11:52:44 +1100

    delete old files

--- st-hawkes-adm-only/run-file-adm-only.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for ind in {1..28} ;
-do
-	echo $ind
-	cp generic-job-adm-only.sh job-adm-only-$ind.sh
-	sed -i -e 's/index_value/'"$ind"'/g' job-adm-only-$ind.sh
-	qsub job-adm-only-$ind.sh
-done
